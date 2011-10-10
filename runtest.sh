717fd2d171224b644b5763a4acfea894a7327a70
Yang Su <suyang1+services@gmail.com>
2011-10-09 23:48:12 -0400

    updated runtest.sh

--- runtest.sh
+++ runtest.sh
@@ -1,43 +1,34 @@
 #!/bin/sh
+diff="diff.txt"
 cp tests/* .
 cp disk-scheduler/* .
-mkdir new old
+cp lib/* .
 
-g++ threadold.cc libinterrupt.a test1.cpp -o test1old.o -ldl -m32
-g++ threadold.cc libinterrupt.a test2.cpp -o test2old.o -ldl -m32
-g++ threadold.cc libinterrupt.a test3.cpp -o test3old.o -ldl -m32
-g++ threadold.cc libinterrupt.a test4.cpp -o test4old.o -ldl -m32
-g++ threadold.cc libinterrupt.a diskold.cc -o diskold.o -ldl -m32
+OLD="old"
+NEW="new"
+mkdir $OLD $NEW
 
-./test1old.o > old/test1.out
-./test2old.o > old/test2.out
-./test3old.o > old/test3.out
-./test4old.o > old/test4.out
+test() {
+  g++ thread$1.cc -c -o thread$1.a -m32
+  for i in 1 2 3 4
+  do
+    g++ thread$1.a libinterrupt.a test$i.cpp -o test$i$1.o -ldl -m32
+    ./test$i$1.o > $2/test$i.out
+  done
+  g++ thread$1.a libinterrupt.a diskold.cc -o disk$1.o -ldl -m32
+  for i in 1 2 3 4 5
+  do
+    ./disk$1.o $i disk.in0 disk.in1 disk.in2 disk.in3 disk.in4 > $2/disk$i.out
+  done
+}
 
-./diskold.o 1 disk.in0 disk.in1 disk.in2 disk.in3 disk.in4 > old/disk1.out
-./diskold.o 2 disk.in0 disk.in1 disk.in2 disk.in3 disk.in4 > old/disk2.out
-./diskold.o 3 disk.in0 disk.in1 disk.in2 disk.in3 disk.in4 > old/disk3.out
-./diskold.o 4 disk.in0 disk.in1 disk.in2 disk.in3 disk.in4 > old/disk4.out
-./diskold.o 5 disk.in0 disk.in1 disk.in2 disk.in3 disk.in4 > old/disk5.out
+test $OLD $OLD
+test "" $NEW
 
-g++ thread.cc libinterrupt.a test1.cpp -o test1.o -ldl -m32
-g++ thread.cc libinterrupt.a test2.cpp -o test2.o -ldl -m32
-g++ thread.cc libinterrupt.a test3.cpp -o test3.o -ldl -m32
-g++ thread.cc libinterrupt.a test4.cpp -o test4.o -ldl -m32
-g++ thread.cc libinterrupt.a disk.cc -o disk.o -ldl -m32
-
-./test1.o > new/test1.out
-./test2.o > new/test2.out
-./test3.o > new/test3.out
-./test4.o > new/test4.out
-./disk.o 1 disk.in0 disk.in1 disk.in2 disk.in3 disk.in4 > new/disk1.out
-./disk.o 2 disk.in0 disk.in1 disk.in2 disk.in3 disk.in4 > new/disk2.out
-./disk.o 3 disk.in0 disk.in1 disk.in2 disk.in3 disk.in4 > new/disk3.out
-./disk.o 4 disk.in0 disk.in1 disk.in2 disk.in3 disk.in4 > new/disk4.out
-./disk.o 5 disk.in0 disk.in1 disk.in2 disk.in3 disk.in4 > new/disk5.out
-
-touch diff.txt
-diff new old > diff.txt
-rm test*.o test*.cpp threadold.cc
+touch $diff
+diff new old > $diff
+rm test*.o test*.cpp threadold.cc thread.a threadold.a
 rm disk.* diskold.*
-rm -r new old
+rm interrupt.h libinterrupt.a
+rm -r $OLD $NEW
+more $diff
